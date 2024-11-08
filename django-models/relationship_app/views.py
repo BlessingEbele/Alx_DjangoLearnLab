cb4cb2ffef61d1bfe71c862ba55f91a2702ace70
blessingebele <blessmart.com@gmail.com>
2024-11-09 00:54:53 +0100

    now

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -36,4 +36,4 @@ def register(request):
             return redirect('list_books')
     else:
         form = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'relationship_app/register.html', {'form': form})
