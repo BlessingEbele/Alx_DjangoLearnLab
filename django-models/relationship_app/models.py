eb5c2d5c668f662d43df2f907953f9369d83e9ce
blessingebele <blessmart.com@gmail.com>
2024-11-09 01:29:26 +0100

    saved files

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -16,7 +16,7 @@ class Author(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE#, related_name'Book')
 
     def __str__(self):
         return self.title
