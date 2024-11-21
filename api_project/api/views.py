ebfdbd0b9e6bbf9c53e2ef16d2ba2d81b2a43338
blessingebele <blessmart.com@gmail.com>
2024-11-22 00:52:33 +0100

    Authentication and Permissions in Django REST Framework

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -24,3 +24,25 @@ class BookViewSet(viewsets.BookViewSet):
 class MyModelViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+
+#just strated here now 
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from .permissions import IsAuthorOrReadOnly
+
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly, IsAdminUser]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly,IsAdminUser]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
