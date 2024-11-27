4abe614b92dc78e9fa18fad27e451a6cb7227656
blessingebele <blessmart.com@gmail.com>
2024-11-28 00:31:44 +0100

    new

---
+++ advanced_api_project/api/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.db import models 
+from .models import models
+
+# Register your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ advanced_api_project/api/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+"""
+Author model represents authors in the system.
+Book model represents books written by authors. Each book links to one author.
+"""
