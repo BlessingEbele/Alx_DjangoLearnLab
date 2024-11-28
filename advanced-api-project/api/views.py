c1eb4d20a0330b4fb677a6ecde98f1cf6780b5ee
blessingebele <blessmart.com@gmail.com>
2024-11-28 01:04:38 +0100

    Implemented custom and generic views for CRUD operations

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
 
