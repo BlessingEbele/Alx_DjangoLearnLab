426d73c6161ebee5eee970a54a413d454ccadbaf
blessingebele <blessmart.com@gmail.com>
2024-12-13 22:09:15 +0100

    moved out and back the mother folder

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*a%e-)zid=&9h%nnd0(vv6tk!^&+6=78thwf%be=_+7fnf+8d(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1, localhost']
 
 
 # Application definition
